7b07c07354db397284fbb0a3baba19b9071aa51a
chaerunnisazhr <caazzhr@gmail.com>
2022-04-07 01:08:20 +0700

    ce

---
+++ app/Views/layouts/header.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-5 ">Welcome!</h1>
+    <p class="lead mb-5"> Break Heal Repeat <br/></p>
+    <p>Mampir <a href="https://www.instagram.com/ce.csv/ ">disini</a>.</p>
+</div>
